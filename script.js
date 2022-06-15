645d7440f46e603596fe06418050e2de3492adbe
Luciano Pires <lucianopires98@outlook.com>
2022-06-14 23:16:49 -0300

    for,while,exercicio

---
+++ script.js
@@ -0,0 +1,18 @@
+for (let x = 1; x <= 10; x++) {
+    console.log(x)
+}
+
+let x = 10
+
+while (x <= 1) {
+    console.log(x);
+    x--;
+}
+
+for (let x = 1; x < 100; x += 2) {
+    console.log(x);
+}
+
+for (let i = 0; i <= 100; i += 2) {
+    console.log(i);
+}
